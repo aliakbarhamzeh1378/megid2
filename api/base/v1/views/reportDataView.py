9e9441705156a7fd6ff1cd44e122f3603288f86a
aliakbar <aliakbarhamzeh1378@hotmail.com>
2023-04-15 14:31:58 +0330

    aa

--- api/base/v1/views/reportDataView.py
+++ api/base/v1/views/reportDataView.py
@@ -1,3 +1,5 @@
+import time
+
 from django.conf import settings
 from drf_yasg.utils import swagger_auto_schema
 from elasticsearch_dsl import Search
@@ -23,7 +25,7 @@ class ReportDataView(APIView):
     def get(self, request):
         try:
             es = Elasticsearch()
-
+            b = time.time()
             # specify the index name
             index_name = 'fluentd'
             # create a search request with a match_all query and sort by timestamp
@@ -58,13 +60,13 @@ class ReportDataView(APIView):
             while len(results['hits']['hits']) > 0:
                 # do something with the search hits
                 for hit in results['hits']['hits']:
-                    data.append(hit)
+                    data.append(hit['_source'])
 
                 # get the next page of search hits using the scroll API
                 scroll_id = results['_scroll_id']
                 results = es.scroll(scroll_id=scroll_id, scroll='2m')
 
-            return Response(data=data , data_status=status.HTTP_200_OK,
+            return Response(data={'data': data, 'time_elapsed': time.time() - b}, data_status=status.HTTP_200_OK,
                             message='Get data  successfully',
                             status=status.HTTP_200_OK)
         except Exception as e:
